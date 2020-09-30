f3d933381279d6892203b781bb50e848c100b661
Vinicius Camargo <viniciu_z@hotmail.com>
2020-09-30 19:11:04 +0200

    Initial Project Cleanup
    - Removed Old Code
    - Upgraded App to Androidx, API 30
    - Added Initial App Dependencies
    - Initial Implementation of the App Navigation Flow
    - Fixed Wrong package name for Unit and UI Tests Directories
    
    Signed-off-by: Vinicius Camargo <viniciu_z@hotmail.com>

---
+++ app/src/main/java/app/storytel/candidate/com/MainActivity.kt
@@ -0,0 +1,23 @@
+package app.storytel.candidate.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp() =
+        findNavController(R.id.nav_host_fragment).navigateUp()
+}
