7cfd8a26dd58ef0191412730eb4550b8bdac8ee2
Vinicius Camargo <viniciu_z@hotmail.com>
2020-10-04 15:09:49 +0200

    Add UI Test
    Signed-off-by: Vinicius Camargo <viniciu_z@hotmail.com>

--- app/src/main/java/app/storytel/candidate/com/viewmodel/PostListViewModel.kt
+++ app/src/main/java/app/storytel/candidate/com/viewmodel/PostListViewModel.kt
@@ -1,13 +1,17 @@
 package app.storytel.candidate.com.viewmodel
 
 import android.view.View
+import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import app.storytel.candidate.com.model.PostAndPhoto
 import app.storytel.candidate.com.repository.IRepository
+import io.reactivex.Single
+import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
 import retrofit2.HttpException
@@ -21,7 +25,30 @@ class PostListViewModel(
             getPostsAndPhotos()
         }
     }
-    private val disposable = CompositeDisposable()
+
+    private val subscriber = object : SingleObserver<List<PostAndPhoto>> {
+
+        override fun onSubscribe(d: Disposable) {
+            loadingVisibility.postValue(View.VISIBLE)
+        }
+
+        override fun onSuccess(t: List<PostAndPhoto>) {
+            posts.postValue(t)
+
+            loadingVisibility.postValue(View.GONE)
+            listLayoutVisibility.postValue(View.VISIBLE)
+            noConnVisibility.postValue(View.GONE)
+        }
+
+        override fun onError(e: Throwable) {
+            // If it's not an HttpException means that it was a network error
+            if (e !is HttpException) {
+                noConnVisibility.postValue(View.VISIBLE)
+                listLayoutVisibility.postValue(View.GONE)
+            }
+            loadingVisibility.postValue(View.GONE)
+        }
+    }
 
     val loadingVisibility = MutableLiveData(View.GONE)
     val noConnVisibility = MutableLiveData(View.GONE)
@@ -36,38 +63,9 @@ class PostListViewModel(
     }
 
     private fun getPostsAndPhotos() {
-        disposable.add(
-            repository.getPostAndPhoto()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(object : DisposableSingleObserver<List<PostAndPhoto>>() {
-                    override fun onStart() {
-                        loadingVisibility.postValue(View.VISIBLE)
-                    }
-
-                    override fun onSuccess(t: List<PostAndPhoto>) {
-                        posts.postValue(t)
-
-                        loadingVisibility.postValue(View.GONE)
-                        listLayoutVisibility.postValue(View.VISIBLE)
-                        noConnVisibility.postValue(View.GONE)
-                    }
-
-                    override fun onError(e: Throwable) {
-                        // If it's not an HttpException means that it was a network error
-                        if (e !is HttpException) {
-                            noConnVisibility.postValue(View.VISIBLE)
-                            listLayoutVisibility.postValue(View.GONE)
-                        }
-                        loadingVisibility.postValue(View.GONE)
-                    }
-                })
-        )
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-
-        disposable.dispose()
+        repository.getPostAndPhoto()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(subscriber)
     }
 }
